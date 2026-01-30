7593d1e8ed817a18786b3b1e716a58a07bd76e45
FynnrhrAmherst28 <136636012+FynnHR28@users.noreply.github.com>
2026-01-29 20:17:52 -0500

    Added server side validation to user login, logout, no current method on the client side to manage the session, which is not something I'm super familiar with. Looking to add client side validation with react-hook-form next

--- graphql/users/user_functions.js
+++ graphql/users/user_functions.js
@@ -1,17 +1,40 @@
 import pool from '../../db/database.js';
 import { hashPassword, checkPasswords } from '../../auth/auth.js';
 
+/* User related functions:
+ Used internally and by user resolvers
+ TODO: abstract database hits, set up database wrapper for query building, abstract server side field validation
+*/
+
+// function used by createUser resolver, some fields may be null, TODO: abstract server side validators 
 export const createUser = async (username, password, email, city, state, country, timezone) => {
     console.log(`Creating user: ${username}, ${email}`);
     const client = await pool.connect();
     console.log(`Database client connected for createUser`);
+
+    // cannot have multiple emails
+    const dup_email = await client.query(`
+        SELECT COUNT(1) FROM public.user WHERE email = LOWER($1)
+    `, [email])
+    if(dup_email.rows[0].count > 0){
+        throw new Error('Email is unavailable (case insensitive), please choose another')
+    }
+
+    // cannot have multiple usernames
+    const dup_username = await client.query(`
+        SELECT COUNT(1) FROM public.user WHERE username = LOWER($1)
+    `, [username])
+    if(dup_username.rows[0].count > 0){
+        throw new Error('Username is unavailable (case insensitive), please choose another')
+    }
+    
     try {
         const passwordHash = await hashPassword(password);
         const response = await client.query(`
             INSERT INTO public.user 
-            (username, password_hash, email, city, state, country, timezone, is_active, created_at, updated_at)
+            (username, password_hash, email, city, state, country, timezone, is_active, created_at, updated_at, last_login)
             VALUES
-            ($1, $2, $3, $4, $5, $6, $7, TRUE, NOW(), NOW())
+            ($1, $2, $3, $4, $5, $6, $7, TRUE, NOW(), NOW(), NOW())
             RETURNING *;
         `,
         [username, passwordHash, email, city, state, country, timezone]);
@@ -19,8 +42,8 @@ export const createUser = async (username, password, email, city, state, country
         return response.rows[0];
     }
     catch (err){
-        console.error(`Error thrown by db during createUser ${ err }`)
-        throw new Error(`Database error while creating user: ${ err.message }`)
+        console.error(`Error thrown during createUser ${ err }`)
+        throw new Error(`${err.message }`)
     }
     finally {
         client.release()
@@ -28,6 +51,7 @@ export const createUser = async (username, password, email, city, state, country
 };
 
 export const getUser = async (id) => {
+    console.log(`Attempting to retrieve user with id: ${id}`)
     const client = await pool.connect();
     try {
         const response = await client.query(`
@@ -59,14 +83,66 @@ export const getUserByEmail = async (email) => {
     }
 };
 
+// Currently used for login resolver logic
 export const verifyUser = async (email, password) => {
+
     const user = await getUserByEmail(email);
+    // User email must exist
     if (!user) {
-        throw new Error('Invalid email or password');
+        throw new Error('Invalid email');
     }
+    // User must be inactive
+    if (user.is_active){
+        throw new Error('User is already active, cannot log in')
+    }
+    // Passwords must match
     const isValid = await checkPasswords(password, user.password_hash);
     if (!isValid) {
-        throw new Error('Invalid email or password');
+        throw new Error('Incorrect password, please try again');
     }
+    // Making sure to update user metadata upon successful login
+    const client = await pool.connect();
+    const response = await client.query(`
+            UPDATE public.user 
+            SET 
+            last_login = NOW(),
+            is_active = TRUE,
+            updated_at = NOW()
+            WHERE id = $1
+    `,
+    [user.id]);
+    client.release()
+
     return user;
 };
+
+
+export const logout = async (id) => {
+    const client = await pool.connect();
+
+    try {
+        const user = await getUser(id)
+        console.log(`Attempting to logout user: ${user.id}`)
+        if(!user.is_active) {
+            throw new Error('User is not active, cannot log out')
+        }
+        // If user is active, update metadata
+        const response = await client.query(`
+            UPDATE public.user 
+            SET 
+            is_active = FALSE,
+            updated_at = NOW()
+            WHERE id = $1
+            RETURNING id, is_active
+        `,
+        [id]);
+        return true;
+    } catch (err) {
+        console.error(`Error thrown by db during logout ${ err }`)
+        throw new Error(`${err.message }`)
+    } finally {
+        client.release()
+    }
+}
+
+
--- graphql/users/user_resolvers.js
+++ graphql/users/user_resolvers.js
@@ -1,4 +1,4 @@
-import { getUser, createUser, verifyUser } from "./user_functions.js";
+import { getUser, createUser, verifyUser, logout } from "./user_functions.js";
 import { timestampsToDateResolver } from "../globals/global_res.js";
 
 
@@ -11,7 +11,11 @@ export default {
     Mutation: {
         createUser: (_, { username, password, email, city, state, country, timezone }) =>
             createUser(username, password, email, city, state, country, timezone),
-        login: (_, { email, password }) => verifyUser(email, password)
+
+        login: (_, { email, password }) => verifyUser(email, password),
+
+        logout: (_, {id}) => logout(id)
+
     },
 
     User: {
